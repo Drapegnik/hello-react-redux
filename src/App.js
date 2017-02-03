e4fee3fd67be1d96165db285b237deaafeb438f7
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-02-03 17:09:34 +0300

    add react-hello-world with hot-reload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Drapegnik on 03.02.17.
+ */
+
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render() {
+        return (<h1>Hello, world.</h1>);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Drapegnik on 03.02.17.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App';
+
+render(<App />, document.getElementById("root"));
